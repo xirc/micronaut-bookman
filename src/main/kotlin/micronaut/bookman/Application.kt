b8743994c6d99280d5fc8eb50ac460547422d06e
xirc <xirckey@gmail.com>
2020-05-10 13:59:11 +0900

    OpenAPI 3.0 Specification ファイルを生成する

--- src/main/kotlin/micronaut/bookman/Application.kt
+++ src/main/kotlin/micronaut/bookman/Application.kt
@@ -1,7 +1,16 @@
 package micronaut.bookman
 
 import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 
+@OpenAPIDefinition(
+        info = Info(
+                title = "Book Management",
+                version = "0.1",
+                description = "Book Management API"
+        )
+)
 object Application {
 
     @JvmStatic
